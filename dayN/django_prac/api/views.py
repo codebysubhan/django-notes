a7995b2e94e8c0843a584fcdfc17a9122f31bb35
codebysubhan <bsdsf22m030@pucit.edu.pk>
2025-07-11 16:13:45 +0500

    updated

--- dayN/django_prac/api/views.py
+++ dayN/django_prac/api/views.py
@@ -2,6 +2,8 @@ import io
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
+from yaml import serialize
+
 from .models import Student
 from .serializer import StudentSerializer
 from django.shortcuts import render
@@ -34,6 +36,20 @@ def student_api(request):
                 'response': 'success'
             }
             json_data = JSONRenderer().render(json_data)
+            return HttpResponse(json_data, content_type='application/json')
+        json_data = serializer.errors
+        return HttpResponse(json_data, content_type='application/json')
+    if request.method == 'PUT':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id')
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu, data=python_data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data updated success'}
+            json_data = JSONRenderer().render(res)
             return HttpResponse(json_data, content_type='application/json')
         json_data = serializer.errors
         return HttpResponse(json_data, content_type='application/json')
--- dayN/django_prac/myapp.py
+++ dayN/django_prac/myapp.py
@@ -1,5 +1,7 @@
 import requests, json
 URL = "http://127.0.0.1:8000/studentapi/"
+
+# READ
 def get_student(id=None):
     data = {}
     if id != None:
@@ -8,7 +10,8 @@ def get_student(id=None):
     r = requests.get(url=URL, data=json_data)
     result = r.json()
     print(result, type(result))
-def create_student():
+# POST
+def post_data():
     data = {
         'name': 'abc',
         'roll': 104,
@@ -19,5 +22,21 @@ def create_student():
     # print(r.text, '----------------------------------------')
     data = r.json()
     print(data)
-create_student()
-# get_student(1)
+# UPDATE
+def update_data():
+    data = {
+        'id': 3,
+        'name': 'changed', # NOTE: roll is not given here
+        'city': 'cityChanged'
+    }
+    json_data = json.dumps(data)
+    r = requests.put(url=URL, data=json_data)
+    # print(r.text, '----------------------------------------')
+    data = r.json()
+    print(data)
+
+
+
+# post_data()
+# get_student()
+update_data()
